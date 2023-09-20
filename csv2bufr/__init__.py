56d6876c8fab5e4cff14189703a2f2966181d4b5
david-i-berry <dberry@wmo.int>
2023-09-20 22:36:23 +0200

    and back to dev.

--- csv2bufr/__init__.py
+++ csv2bufr/__init__.py
@@ -19,7 +19,7 @@
 #
 ###############################################################################
 
-__version__ = '0.7.4'
+__version__ = '0.7.dev5'
 
 import csv
 from datetime import timezone, datetime
