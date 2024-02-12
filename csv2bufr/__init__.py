586673f88079c2d126eb1cfad4669a79b458fd7f
david-i-berry <dberry@wmo.int>
2024-02-12 13:54:51 +0100

    V0.8.0 (#129)
    * Bump version number
    
    * Bump version number to 0.8.0, some potentially breaking changes.

--- csv2bufr/__init__.py
+++ csv2bufr/__init__.py
@@ -19,7 +19,7 @@
 #
 ###############################################################################
 
-__version__ = '0.7.dev5'
+__version__ = '0.8.0'
 
 import csv
 from datetime import timezone, datetime
