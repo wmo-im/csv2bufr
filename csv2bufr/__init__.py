905c333acdf1cafd15405f39d412ac155b1fa5de
david-i-berry <dberry@wmo.int>
2024-03-25 15:37:23 +0100

    Update to version # ready for release.

--- csv2bufr/__init__.py
+++ csv2bufr/__init__.py
@@ -19,7 +19,7 @@
 #
 ###############################################################################
 
-__version__ = '0.8.2_dev'
+__version__ = '0.8.2'
 
 import csv
 from datetime import timezone, datetime
